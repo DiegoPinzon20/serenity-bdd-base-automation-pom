3c8330fc604ec49ae5e5ee8b48776989dd6ace62
DiegoPinzon20 <diegopip62@gmail.com>
2023-03-07 22:09:56 -0500

    Se actualiza la referencia al paquete de definiciones.

--- src/test/java/com/reliquias/software/runners/LoginRunner.java
+++ src/test/java/com/reliquias/software/runners/LoginRunner.java
@@ -7,13 +7,13 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/inicio_sesion.feature",
-        glue = "com.reliquias.software.stepdefinitions",
+        glue = "com.reliquias.software.definitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         plugin = {
                 "pretty",
-                "json:target/serenity-reports/web_applications/login/cucumber.json",
-                "html:target/serenity-reports/web_applications/login/serenity-html-report.html",
-                "rerun:target/serenity-reports/web_applications/login/rerun.txt"
+                "json:target/serenity-reports/login/cucumber.json",
+                "html:target/serenity-reports/login/serenity-html-report.html",
+                "rerun:target/serenity-reports/login/rerun.txt"
         }
 )
 public class LoginRunner {
