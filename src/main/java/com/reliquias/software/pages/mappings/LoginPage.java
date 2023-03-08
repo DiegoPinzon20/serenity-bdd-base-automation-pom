d9ebd3d1aa5ce3f80927379bb33cff5428550fa5
DiegoPinzon20 <diegopip62@gmail.com>
2023-03-07 21:26:19 -0500

    Se actualiza el nombre de las paginas.

--- src/main/java/com/reliquias/software/pages/mappings/LoginPage.java
+++ src/main/java/com/reliquias/software/pages/mappings/LoginPage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
-public class PaginaInicioSesion extends PageObject {
+public class LoginPage extends PageObject {
 
     @FindBy(xpath = "//input[@name = 'Username']")
     WebElementFacade inputUsername;
--- src/main/java/com/reliquias/software/pages/mappings/MainPage.java
+++ src/main/java/com/reliquias/software/pages/mappings/MainPage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
-public class MenuPaginaPrincipal extends PageObject {
+public class MainPage extends PageObject {
 
     @FindBy(xpath = "//main/section/section/h1")
     WebElementFacade txtTitleMainPage;
